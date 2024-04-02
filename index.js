6f6c2ae9a3e39ca6968bf2bf55866fc3d70e97fc
Jinkc21 <jin.k.choe@gmail.com>
2024-04-02 17:21:02 -0400

    added code to pass data between prompt and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
+// defining some dataset
 const questions = [
 {
     type: 'input',
@@ -11,37 +15,37 @@ const questions = [
 {   type: 'input',
     message: 'What was your motivation for the project?',
     name: 'what',
-}
+},
 {
     type: 'input',
     message: 'Why did you build this project?',
     name: 'why',
-}
+},
 {
     type: 'input',
     message: 'How is this project useful?',
     name: 'how',
-}
+},
 {
     type: 'input',
     message: 'What are the steps required to install your project?',
     name: 'installation',
-}
+},
 {
     type: 'input',
     message: 'Provide instructions and examples of your project being used.',
     name: 'usage',
-}
+},
 {
     type: 'input',
     message: 'List your collaborators and provide links any parties that helped with your project.',
     name: 'credits',
-}
+},
 {
-    type: 'input',
+    type: 'checkbox',
     message: 'Select an option for your license.',
     name: 'license',
-    choices: 
+    choices: ['Apache', 'MIT', 'Boost', 'Elipse', 'Mozilla']
 }
     
 ];
@@ -49,14 +53,40 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
+    // we want to take the USER data and WRITE it to a NEW FILE
 
-
+    // What is the name of our file?
+    // Where is the data comming from
+    // HOW to we FORMAT a WRITE PROCEEDURE
+    fs.writeFile(fileName, data, function(error) {
+        if(error) {
+            console.log("Error: ", error);
+        }
+        console.log("New File Created");
+    });
 }
 
 // TODO: Create a function to initialize app
 function init() {
-
-
+    // console.log("I am code BEFORE the QUESTIONS Set")
+    // here we kick our questions to the user  --> ASYNC OPERATION
+    inquirer.prompt(questions)
+        .then(function(data) {
+           // console.log("I am code INSIDE the RETRUED RESPONSE")
+            console.log(data);
+            
+            let result = generateMarkdown(data);
+            console.log('Reulsts: ', result);
+            writeToFile('README.md', result);
+            // writeToFile()
+        })
+        .catch(error => {
+            
+        })
+        
+      //  generateMarkdown(data);
+  //  console.log("I am code AFTER the QUESTIONS Set")
+  //  console.log("Data is no longer available at this point")
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,11 +16,39 @@ function renderLicenseSection(license) {
   
 }
 
+/*
+function noInputs() {
+  let someData = [1, 2, 3, 4, 5];
+
+  renderLicenseBadge("MIT");
+  renderLicenseLink()
+  return 5;
+}
+*/
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log("Data In Markdown Function: ", data);
   return `# ${data.title}
 
+## Description
+${data.what}
+${data.why}
+${data.how}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## License
+${data.license}
 `;
+
 }
 
 module.exports = generateMarkdown;
